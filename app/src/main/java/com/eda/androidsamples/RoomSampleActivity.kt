ce24af981ddb1c50e056b7a348982d6ad5fa66db
Ryo Kobayashi <kobayashi@eda-inc.jp>
2018-08-13 17:00:28 +0900

    Gradleプラグイン・ライブラリバージョン更新

--- app/src/main/java/com/eda/androidsamples/RoomSampleActivity.kt
+++ app/src/main/java/com/eda/androidsamples/RoomSampleActivity.kt
@@ -24,161 +24,159 @@ import io.reactivex.schedulers.Schedulers
  */
 class RoomSampleActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRoomSampleBinding
-    private lateinit var db: SampleDatabase
-    private lateinit var adapter: Adapter
+  private lateinit var binding: ActivityRoomSampleBinding
+  private lateinit var db: SampleDatabase
+  private lateinit var adapter: Adapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_room_sample)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_room_sample)
 
-        db = SampleDatabase.create(this, "sample.db")
-        // テストユーザーを追加
+    db = SampleDatabase.create(this, "sample.db")
+    // テストユーザーを追加
 //        Single.just(User(userId = "1", name = "テストユーザー", age = 20))
 //            .map { db.userDao().upsert(it) }
 //            .subscribeOn(Schedulers.io())
 //            .subscribe()
 
-        // Listアダプター生成
-        adapter = Adapter(this)
-        binding.list.adapter = adapter
-        // 移動・削除コールバック
-        val helper = ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(
-            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
-            ItemTouchHelper.RIGHT) {
-            override fun onMove(recyclerView: RecyclerView?,
-                                viewHolder: RecyclerView.ViewHolder?,
-                                target: RecyclerView.ViewHolder?): Boolean {
-                if(viewHolder == null || target == null) return false
-                val fromPos = viewHolder.adapterPosition
-                val toPos = target.adapterPosition
-                adapter.notifyItemMoved(fromPos, toPos)
-                return true
-            }
+    // Listアダプター生成
+    adapter = Adapter(this)
+    binding.list.adapter = adapter
+    // 移動・削除コールバック
+    val helper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.RIGHT) {
+      override fun onMove(recyclerView: RecyclerView,
+                          viewHolder: RecyclerView.ViewHolder,
+                          target: RecyclerView.ViewHolder): Boolean {
+        val fromPos = viewHolder.adapterPosition
+        val toPos = target.adapterPosition
+        adapter.notifyItemMoved(fromPos, toPos)
+        return true
+      }
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder?,
-                                  direction: Int) {
-                if(viewHolder == null) return
-                val fromPos = viewHolder.adapterPosition
-                Single.just(adapter.removeItem(fromPos))
-                    .map { db.userDao().delete(it) }
-                    .subscribeOn(Schedulers.io())
-                    .subscribe()
-            }
-        })
-        helper.attachToRecyclerView(binding.list)
-        binding.list.addItemDecoration(helper)
-
-        // データ取得
-        val userDao = db.userDao()
-        userDao.getAll()
+      override fun onSwiped(viewHolder: RecyclerView.ViewHolder,
+                            direction: Int) {
+        val fromPos = viewHolder.adapterPosition
+        Single.just(adapter.removeItem(fromPos))
+            .map { db.userDao().delete(it) }
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                adapter.mergeItems(it)
-            }
+            .subscribe()
+      }
+    })
+    helper.attachToRecyclerView(binding.list)
+    binding.list.addItemDecoration(helper)
 
-        binding.addUser.setOnClickListener {
-            Single.just(User(userId = System.currentTimeMillis().toString(), name = "テストユーザー", age = 20))
-                .map { db.userDao().upsert(it) }
-                .subscribeOn(Schedulers.io())
-                .subscribe()
+    // データ取得
+    val userDao = db.userDao()
+    userDao.getAll()
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe {
+          adapter.mergeItems(it)
         }
+
+    binding.addUser.setOnClickListener {
+      Single.just(User(userId = System.currentTimeMillis().toString(), name = "テストユーザー", age = 20))
+          .map { db.userDao().upsert(it) }
+          .subscribeOn(Schedulers.io())
+          .subscribe()
+    }
+  }
+
+  // RecyclerView Adapter
+  class Adapter(
+      context: Context,
+      private val inflater: LayoutInflater = LayoutInflater.from(context),
+      private val items: MutableList<User> = mutableListOf()
+  ) : RecyclerView.Adapter<ViewHolder>() {
+
+    fun setItems(newItems: List<User>) {
+      items.clear()
+      items.addAll(newItems)
+      notifyDataSetChanged()
     }
 
-    // RecyclerView Adapter
-    class Adapter(
-        context: Context,
-        private val inflater: LayoutInflater = LayoutInflater.from(context),
-        private val items: MutableList<User> = mutableListOf()
-    ) : RecyclerView.Adapter<ViewHolder>() {
-
-        fun setItems(newItems: List<User>) {
-            items.clear()
-            items.addAll(newItems)
-            notifyDataSetChanged()
+    fun mergeItems(incoming: List<User>) {
+      var userChanged = false
+      val newUsers: MutableList<User> = mutableListOf()
+      incoming.forEach {
+        val index = getIndexByUserId(it.userId)
+        if (index != -1) {
+          items[index] = it
+          userChanged = true
+        } else {
+          newUsers.add(it)
         }
+      }
 
-        fun mergeItems(incoming: List<User>) {
-            var userChanged = false
-            val newUsers: MutableList<User> = mutableListOf()
-            incoming.forEach {
-                val index = getIndexByUserId(it.userId)
-                if(index != -1) {
-                    items[index] = it
-                    userChanged = true
-                } else {
-                    newUsers.add(it)
-                }
-            }
+      if (!newUsers.isEmpty()) {
+        items.addAll(newUsers)
+        userChanged = true
+      }
 
-            if(!newUsers.isEmpty()) {
-                items.addAll(newUsers)
-                userChanged = true
-            }
-
-            if(userChanged) {
-                notifyDataSetChanged()
-            }
-        }
-
-        fun mergeItem(user: User) {
-            val index = getIndexByUserId(user.userId)
-            if(index != -1) {
-                items[index] = user
-                notifyItemChanged(index)
-            } else {
-                items.add(user)
-                notifyItemInserted(index)
-            }
-        }
-
-        fun removeItem(position: Int): User? {
-            var user: User? = null
-            if(position != itemCount) {
-                user = items.removeAt(position)
-                notifyItemRemoved(position)
-            }
-            return user
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(inflater.inflate(R.layout.view_user_item, parent, false))
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.setItem(items[position])
-        }
-
-        override fun getItemCount(): Int {
-            return items.size
-        }
-
-        private fun getUserById(user: User): User? {
-            items.forEach {
-                if(it.userId == user.userId) return it
-            }
-            return null
-        }
-
-        private fun getIndexByUserId(userId: String): Int {
-            var i = 0
-            items.forEach {
-                if(it.userId == userId) return i
-                i++
-            }
-            return -1
-        }
+      if (userChanged) {
+        notifyDataSetChanged()
+      }
     }
 
-    // RecyclerView ViewHolder
-    class ViewHolder(
-        itemView: View,
-        private val binding: ViewUserItemBinding? = DataBindingUtil.bind(itemView)
-    ): RecyclerView.ViewHolder(itemView) {
-
-        fun setItem(item: User) {
-            binding?.item = item
-        }
+    fun mergeItem(user: User) {
+      val index = getIndexByUserId(user.userId)
+      if (index != -1) {
+        items[index] = user
+        notifyItemChanged(index)
+      } else {
+        items.add(user)
+        notifyItemInserted(index)
+      }
     }
+
+    fun removeItem(position: Int): User? {
+      var user: User? = null
+      if (position != itemCount) {
+        user = items.removeAt(position)
+        notifyItemRemoved(position)
+      }
+      return user
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+      return ViewHolder(inflater.inflate(R.layout.view_user_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+      holder.setItem(items[position])
+    }
+
+    override fun getItemCount(): Int {
+      return items.size
+    }
+
+    private fun getUserById(user: User): User? {
+      items.forEach {
+        if (it.userId == user.userId) return it
+      }
+      return null
+    }
+
+    private fun getIndexByUserId(userId: String): Int {
+      var i = 0
+      items.forEach {
+        if (it.userId == userId) return i
+        i++
+      }
+      return -1
+    }
+  }
+
+  // RecyclerView ViewHolder
+  class ViewHolder(
+      itemView: View,
+      private val binding: ViewUserItemBinding? = DataBindingUtil.bind(itemView)
+  ) : RecyclerView.ViewHolder(itemView) {
+
+    fun setItem(item: User) {
+      binding?.item = item
+    }
+  }
 }
--- app/src/main/java/com/eda/androidsamples/palette/PaletteDetailFragment.kt
+++ app/src/main/java/com/eda/androidsamples/palette/PaletteDetailFragment.kt
@@ -19,35 +19,38 @@ import com.eda.androidsamples.databinding.FragmentPaletteDetailBinding
  */
 class PaletteDetailFragment : Fragment() {
 
-    private lateinit var binding: FragmentPaletteDetailBinding
+  private lateinit var binding: FragmentPaletteDetailBinding
 
-    companion object {
-        private val KEY_RESOURCE_ID = "res_id"
+  companion object {
+    private val KEY_RESOURCE_ID = "res_id"
 
-        fun newInstance(@DrawableRes resId: Int): PaletteDetailFragment {
-            val f = PaletteDetailFragment()
-            val args = Bundle()
-            args.putInt(KEY_RESOURCE_ID, resId)
-            f.arguments = args
-            return f
-        }
+    fun newInstance(@DrawableRes resId: Int): PaletteDetailFragment {
+      val f = PaletteDetailFragment()
+      val args = Bundle()
+      args.putInt(KEY_RESOURCE_ID, resId)
+      f.arguments = args
+      return f
+    }
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_palette_detail, container, false)
+
+    val resId = arguments!!.getInt(KEY_RESOURCE_ID)
+    binding.image.setImageResource(resId)
+    val bmp = BitmapFactory.decodeResource(resources, resId)
+    Palette.from(bmp).generate {
+      if (it == null) {
+        return@generate
+      }
+      binding.vibrant.setBackgroundColor(it.getVibrantColor(0x888888))
+      binding.vibrantDark.setBackgroundColor(it.getDarkVibrantColor(0x888888))
+      binding.vibrantLight.setBackgroundColor(it.getLightVibrantColor(0x888888))
+      binding.muted.setBackgroundColor(it.getMutedColor(0x888888))
+      binding.mutedDark.setBackgroundColor(it.getDarkMutedColor(0x888888))
+      binding.mutedLight.setBackgroundColor(it.getLightMutedColor(0x888888))
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_palette_detail, container, false)
-
-        val resId = arguments!!.getInt(KEY_RESOURCE_ID)
-        binding.image.setImageResource(resId)
-        val bmp = BitmapFactory.decodeResource(resources, resId)
-        Palette.from(bmp).generate {
-            binding.vibrant.setBackgroundColor(it.getVibrantColor(0x888888))
-            binding.vibrantDark.setBackgroundColor(it.getDarkVibrantColor(0x888888))
-            binding.vibrantLight.setBackgroundColor(it.getLightVibrantColor(0x888888))
-            binding.muted.setBackgroundColor(it.getMutedColor(0x888888))
-            binding.mutedDark.setBackgroundColor(it.getDarkMutedColor(0x888888))
-            binding.mutedLight.setBackgroundColor(it.getLightMutedColor(0x888888))
-        }
-
-        return binding.root
-    }
+    return binding.root
+  }
 }
