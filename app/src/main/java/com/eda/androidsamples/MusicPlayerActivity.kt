79faa00e689686dbb53a165ba4f4b847b3167274
Ryo Kobayashi <kobayashi@eda-inc.jp>
2018-08-24 11:21:29 +0900

    音楽プレイヤー画面ソースフォーマット

--- app/src/main/java/com/eda/androidsamples/MusicPlayerActivity.kt
+++ app/src/main/java/com/eda/androidsamples/MusicPlayerActivity.kt
@@ -25,177 +25,177 @@ import java.util.concurrent.TimeUnit
 
 class MusicPlayerActivity : AppCompatActivity() {
 
-    private lateinit var mediaSession: MediaSessionCompat
-    private lateinit var stateBuilder: PlaybackStateCompat.Builder
-    private var mp: MediaPlayer? = null
-    private var disposable: Disposable? = null
-    private lateinit var seekBar: SeekBar
-    private lateinit var current: TextView
-    private lateinit var time: TextView
-    private var isControlling = false
+  private lateinit var mediaSession: MediaSessionCompat
+  private lateinit var stateBuilder: PlaybackStateCompat.Builder
+  private var mp: MediaPlayer? = null
+  private var disposable: Disposable? = null
+  private lateinit var seekBar: SeekBar
+  private lateinit var current: TextView
+  private lateinit var time: TextView
+  private var isControlling = false
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_music_player)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_music_player)
 
-        // media session作成
-        mediaSession = MediaSessionCompat(this, "AS - MusicPlayer")
-        mediaSession.setFlags(
-            MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or
-                MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS)
-        mediaSession.setCallback(object: MediaSessionCompat.Callback() {
-            override fun onPlay() {
-                setPlaying(true)
-            }
+    // media session作成
+    mediaSession = MediaSessionCompat(this, "AS - MusicPlayer")
+    mediaSession.setFlags(
+        MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or
+            MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS)
+    mediaSession.setCallback(object : MediaSessionCompat.Callback() {
+      override fun onPlay() {
+        setPlaying(true)
+      }
 
-            override fun onPause() {
-                setPlaying(false)
-            }
-        },
-            Handler(Looper.getMainLooper()))
+      override fun onPause() {
+        setPlaying(false)
+      }
+    },
+        Handler(Looper.getMainLooper()))
 
-        mp = MediaPlayer.create(this, R.raw.jazz_in_paris)
+    mp = MediaPlayer.create(this, R.raw.jazz_in_paris)
 
-        // メディアの更新を検知
-        seekBar = findViewById(R.id.seek)
-        current = findViewById(R.id.current)
-        time = findViewById(R.id.time)
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if(!fromUser) return
-                seekBar?.let { bar ->
-                    updateUi(bar.progress, bar.max)
-                }
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-                isControlling = true
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-                isControlling = false
-                p0?.let { bar ->
-                    mp?.let { p ->
-                        p.seekTo(bar.progress)
-                        if(p.isPlaying) {
-                            p.start()
-                        }
-                    }
-                }
-            }
-        })
-
-        // 再生・停止
-        findViewById<Button>(R.id.play).setOnClickListener {
-            togglePlay()
-            mediaSession.isActive = true
+    // メディアの更新を検知
+    seekBar = findViewById(R.id.seek)
+    current = findViewById(R.id.current)
+    time = findViewById(R.id.time)
+    seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+      override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        if (!fromUser) return
+        seekBar?.let { bar ->
+          updateUi(bar.progress, bar.max)
         }
+      }
 
-    }
+      override fun onStartTrackingTouch(p0: SeekBar?) {
+        isControlling = true
+      }
 
-    override fun onStart() {
-        super.onStart()
-        startUpdateUi()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        stopUpdateUi()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaSession.release()
-        mp?.release()
-        mp = null
-        disposable?.dispose()
-        disposable = null
-    }
-
-    private fun startUpdateUi() {
-        disposable = Observable.interval(32, 32, TimeUnit.MILLISECONDS)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                if(isControlling) return@subscribe
-                mp?.let { p ->
-                    updateUi(p.currentPosition, p.duration)
-                }
+      override fun onStopTrackingTouch(p0: SeekBar?) {
+        isControlling = false
+        p0?.let { bar ->
+          mp?.let { p ->
+            p.seekTo(bar.progress)
+            if (p.isPlaying) {
+              p.start()
             }
-    }
-
-    private fun stopUpdateUi() {
-        disposable?.dispose()
-        disposable = null
-    }
-
-    private fun updateUi(currentPosition: Int, duration: Int) {
-        seekBar.progress = currentPosition
-        seekBar.max = duration
-
-        val csecond = currentPosition / 1000
-        val cminutes = csecond / 60
-        val csec = csecond % 60
-        current.text = "%02d:%02d".format(cminutes, csec)
-
-        val second = duration / 1000
-        val minutes = second / 60
-        val sec = second % 60
-        time.text = "%02d:%02d".format(minutes, sec)
-    }
-
-    private fun setPlaying(play: Boolean) {
-        mp?.let { player ->
-            val btn = findViewById<Button>(R.id.play)
-            if(play) {
-                player.start()
-                btn.text = "停止"
-                updateMediaState(false)
-            } else {
-                player.pause()
-                btn.text = "再生"
-                updateMediaState(true)
-            }
+          }
         }
+      }
+    })
+
+    // 再生・停止
+    findViewById<Button>(R.id.play).setOnClickListener {
+      togglePlay()
+      mediaSession.isActive = true
     }
 
-    private fun togglePlay() {
-        mp?.let { player ->
-            val btn = findViewById<Button>(R.id.play)
-            if(player.isPlaying) {
-                player.pause()
-                btn.text = "再生"
-                updateMediaState(true)
-            } else {
-                player.start()
-                btn.text = "停止"
-                updateMediaState(false)
-            }
-        }
-    }
+  }
 
-    private fun updateMediaState(isPlaying: Boolean) {
-        stateBuilder = PlaybackStateCompat.Builder()
-            .setActions(
-                PlaybackStateCompat.ACTION_PLAY or
-                    PlaybackStateCompat.ACTION_PAUSE or
-                    PlaybackStateCompat.ACTION_PLAY_PAUSE)
-            .setState(
-                if(isPlaying) PlaybackStateCompat.STATE_PAUSED else PlaybackStateCompat.STATE_PLAYING,
-                0,
-                1.0f
-            )
-        mediaSession.setPlaybackState(stateBuilder.build())
-    }
+  override fun onStart() {
+    super.onStart()
+    startUpdateUi()
+  }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return super.onKeyDown(keyCode, event)
+  override fun onStop() {
+    super.onStop()
+    stopUpdateUi()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mediaSession.release()
+    mp?.release()
+    mp = null
+    disposable?.dispose()
+    disposable = null
+  }
+
+  private fun startUpdateUi() {
+    disposable = Observable.interval(32, 32, TimeUnit.MILLISECONDS)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe {
+          if (isControlling) return@subscribe
+          mp?.let { p ->
+            updateUi(p.currentPosition, p.duration)
+          }
         }
-        when(keyCode) {
-            KeyEvent.KEYCODE_MEDIA_PLAY -> {
-                return true
-            }
-        }
-        return super.onKeyDown(keyCode, event)
+  }
+
+  private fun stopUpdateUi() {
+    disposable?.dispose()
+    disposable = null
+  }
+
+  private fun updateUi(currentPosition: Int, duration: Int) {
+    seekBar.progress = currentPosition
+    seekBar.max = duration
+
+    val csecond = currentPosition / 1000
+    val cminutes = csecond / 60
+    val csec = csecond % 60
+    current.text = "%02d:%02d".format(cminutes, csec)
+
+    val second = duration / 1000
+    val minutes = second / 60
+    val sec = second % 60
+    time.text = "%02d:%02d".format(minutes, sec)
+  }
+
+  private fun setPlaying(play: Boolean) {
+    mp?.let { player ->
+      val btn = findViewById<Button>(R.id.play)
+      if (play) {
+        player.start()
+        btn.text = "停止"
+        updateMediaState(false)
+      } else {
+        player.pause()
+        btn.text = "再生"
+        updateMediaState(true)
+      }
     }
+  }
+
+  private fun togglePlay() {
+    mp?.let { player ->
+      val btn = findViewById<Button>(R.id.play)
+      if (player.isPlaying) {
+        player.pause()
+        btn.text = "再生"
+        updateMediaState(true)
+      } else {
+        player.start()
+        btn.text = "停止"
+        updateMediaState(false)
+      }
+    }
+  }
+
+  private fun updateMediaState(isPlaying: Boolean) {
+    stateBuilder = PlaybackStateCompat.Builder()
+        .setActions(
+            PlaybackStateCompat.ACTION_PLAY or
+                PlaybackStateCompat.ACTION_PAUSE or
+                PlaybackStateCompat.ACTION_PLAY_PAUSE)
+        .setState(
+            if (isPlaying) PlaybackStateCompat.STATE_PAUSED else PlaybackStateCompat.STATE_PLAYING,
+            0,
+            1.0f
+        )
+    mediaSession.setPlaybackState(stateBuilder.build())
+  }
+
+  override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      return super.onKeyDown(keyCode, event)
+    }
+    when (keyCode) {
+      KeyEvent.KEYCODE_MEDIA_PLAY -> {
+        return true
+      }
+    }
+    return super.onKeyDown(keyCode, event)
+  }
 }
