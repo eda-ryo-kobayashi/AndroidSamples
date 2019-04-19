80066365d2969d09f7a9c240b19ec22ef15e8938
Ryo Kobayashi <kobayashi@eda-inc.jp>
2019-04-19 16:49:29 +0900

    コルーチンテスト追加

--- app/src/main/java/com/eda/androidsamples/CoroutineTestActivity.kt
+++ app/src/main/java/com/eda/androidsamples/CoroutineTestActivity.kt
@@ -1,18 +1,22 @@
 package com.eda.androidsamples
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlinx.coroutines.rx2.rxSingle
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by RyoKobayashi on 2018/09/21
  *
  */
-class CoroutineTestActivity : AppCompatActivity() {
+class CoroutineTestActivity : AppCompatActivity(), CoroutineScope {
+
+  private var job = Job()
+  override val coroutineContext: CoroutineContext
+    get() = job + Dispatchers.Main
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -30,11 +34,42 @@ class CoroutineTestActivity : AppCompatActivity() {
         Log.i("CoroutineTestActivity", "main rxSingle ${Thread.currentThread().name}")
         1
       }
-          .observeOn(AndroidSchedulers.mainThread())
+        .observeOn(AndroidSchedulers.mainThread())
       x.subscribe { a ->
         Log.i("CoroutineTestActivity", "main subscribe ${Thread.currentThread().name}")
       }
     }
 
+    executeTest()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    job.cancel()
+  }
+
+  private fun executeTest() {
+    launch {
+      Log.d("start heavy : ", Thread.currentThread().name)
+      heavy()
+      Log.d("end heavy : ", Thread.currentThread().name)
+
+      Log.d("start heavyIO : ", Thread.currentThread().name)
+      heavyIO()
+      Log.d("end heavyIO : ", Thread.currentThread().name)
+    }
+  }
+
+  private suspend fun heavy() {
+    delay(1000)
+    Log.d("heavy Thread : ", Thread.currentThread().name)
+  }
+
+  private fun heavyIO() = launch(Dispatchers.IO) {
+    delay(1000)
+    Log.d("heavyIO Thread : ", Thread.currentThread().name)
+    launch(Dispatchers.Main) {
+      Log.d("heavyIO$1 Thread : ", Thread.currentThread().name)
+    }
   }
 }
